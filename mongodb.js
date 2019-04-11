06cf36f9b5c883ef8d0d683165d97cf3238a25c5
shahin1290 <s.patowary@yahoo.com>
2019-04-11 23:49:48 +0200

    Adds new collection of tasks in MongoDb database

--- mongodb.js
+++ mongodb.js
@@ -1,16 +1,32 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const { MongoClient } = require('mongodb')
+
+
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
   if (error) {
     return console.log('Unable to connect to database!')
   }
-  
+
   const db = client.db(databaseName)
 
   db.collection('users').insertOne({
     name: 'Andrew',
     age: 27
   })
+
+  db.collection('tasks').insertMany([
+    {
+      description: 'Clean the house',
+      completed: true
+    },{
+      description: 'Renew inspection',
+      completed: false
+    }
+    ], (error, result) => {
+      if (error) {
+        return console.log('Unable to insert tasks!')
+      }
+      console.log(result.ops)
+    })
 })
