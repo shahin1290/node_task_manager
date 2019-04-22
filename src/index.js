df4f87704edda961403523a32b868d87ce52ee01
shahin1290 <s.patowary@yahoo.com>
2019-04-22 23:55:34 +0200

    Adds delete endpoint for  user and task resources

--- src/index.js
+++ src/index.js
@@ -58,7 +58,18 @@ app.patch('/users/:id', async(req, res) => {
   } catch (error) {
     res.status(400).send(error)
   }
+})
 
+app.delete('/users/:id', async(req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id)
+    if(!user){
+      return res.status(404).send()
+    }
+    res.send(user)
+  } catch (error) {
+    res.status(500).send(error)
+  }
 })
 
 app.get('/tasks', async(req, res) => {
@@ -117,6 +128,18 @@ app.patch('/tasks/:id', async(req, res) => {
     res.status(400).send(error)
   }
 
+})
+
+app.delete('/tasks/:id', async(req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id)
+    if(!task){
+      return res.status(404).send()
+    }
+    res.send(task)
+  } catch (error) {
+    res.status(500).send(error)
+  }
 })
 
 const port = process.env.PORT || 3000
