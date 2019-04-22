af5ac2ae68119359e6675d1760cca7f81696c329
shahin1290 <s.patowary@yahoo.com>
2019-04-22 02:19:32 +0200

    Adds resources creation endpoints to create task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
+const Task = require('./models/task')
+
 
 const app = express()
 app.use(express.json())
@@ -9,7 +11,18 @@ app.post('/users', (req, res) => {
   const user = new User(req.body)
 
   user.save().then(() => {
-    res.send(user)
+    res.status(201).send(user)
+  }).catch((e) => {
+    res.status(400).send(e)
+  })
+
+})
+
+app.post('/tasks', (req, res) => {
+  const task = new Task(req.body)
+
+  task.save().then(() => {
+    res.status(201).send(task)
   }).catch((e) => {
     res.status(400).send(e)
   })
