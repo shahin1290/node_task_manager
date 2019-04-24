a044119844c76149615469575916439fb392ef16
shahin1290 <s.patowary@yahoo.com>
2019-04-24 18:42:02 +0200

    Sets up bcryptjs and mongoose to hash password before saving

--- src/routers/user.js
+++ src/routers/user.js
@@ -46,11 +46,17 @@ router.patch('/users/:id', async(req, res) => {
   }
 
   try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+    const user = await User.findById(req.params.id)
+
+    updates.forEach((update) => user[update] = req.body[update])
+    await user.save()
+
     if(!user){
       return res.status(404).send()
     }
+
     res.send(user)
+
   } catch (error) {
     res.status(400).send(error)
   }
