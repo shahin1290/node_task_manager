236a488c0e29d31275090cffaeab727a9f709977
shahin1290 <s.patowary@yahoo.com>
2019-04-27 22:48:31 +0200

    Generating and accepting authentication tokens

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -11,6 +12,10 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true,
     validate(value){
       if(!validator.isEmail(value)){
         throw new Error('Please provide valid email')
@@ -28,7 +33,13 @@ const userSchema = new mongoose.Schema({
         throw new Error('Password contains password')
       }
     }
-  }
+  },
+  tokens: [{
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 })
 
 userSchema.pre('save', async function (next) {
@@ -40,6 +51,33 @@ userSchema.pre('save', async function (next) {
 
   next()
 })
+
+userSchema.methods.generateAuthToken = async function() {
+  const user = this
+  const token = jwt.sign({_id: user._id.toString() }, 'mynameisshahin')
+
+  user.tokens = user.tokens.concat({ token })
+
+  await user.save()
+
+  return token
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email })
+
+  if (!user) {
+    throw new Error('Unable to login')
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password)
+
+  if (!isMatch) {
+    throw new Error('Unable to login')
+  }
+
+  return user
+}
 
 const User = mongoose.model('User', userSchema)
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,14 +1,10 @@
 const express = require('express')
 const User = require('../models/user')
 const router = express.Router()
+const auth = require('../middleware/auth')
 
-router.get('/users', async(req, res) => {
-  try {
-    const users = await User.find({})
-    res.send(users)
-  } catch (error) {
-    res.status(500).send()
-  }
+router.get('/users/me', auth, async(req, res) => {
+  res.send(req.user)
 })
 
 router.get('/users/:id', async(req, res) => {
@@ -30,9 +26,21 @@ router.post('/users', async(req, res) => {
 
   try {
     await user.save()
-    res.status(201).send(user)
+    const token = await user.generateAuthToken()
+    res.status(201).send({ user, token })
   } catch (error) {
     res.status(400).send(error)
+  }
+})
+
+router.post('/users/login', async(req, res) => {
+
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password)
+    const token= await user.generateAuthToken()
+    res.send({ user, token })
+  } catch (error) {
+    res.status(400).send()
   }
 })
 
