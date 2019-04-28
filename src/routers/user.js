894914917fc262cc8aecf32a425673adf2174bcd
shahin1290 <s.patowary@yahoo.com>
2019-04-28 18:57:31 +0200

    Adds logout functionality to users

--- src/routers/user.js
+++ src/routers/user.js
@@ -7,6 +7,31 @@ router.get('/users/me', auth, async(req, res) => {
   res.send(req.user)
 })
 
+router.post('/users/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token
+    })
+
+    await req.user.save()
+    res.send()
+  } catch (error) {
+    res.status(500).send()
+  }
+}) 
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+  try {
+    req.user.tokens = []
+
+
+    await req.user.save()
+    res.send()
+  } catch (error) {
+    res.status(500).send()
+  }
+}) 
+
 router.get('/users/:id', async(req, res) => {
   const _id = req.params.id
 
