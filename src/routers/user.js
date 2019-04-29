79d0727c5656ab4f5812519fd66c2742d3792c56
shahin1290 <s.patowary@yahoo.com>
2019-04-30 00:30:01 +0200

    Adds methods to serialize user data for hiding private data and refactors some endpoints

--- src/routers/user.js
+++ src/routers/user.js
@@ -32,20 +32,6 @@ router.post('/users/logoutAll', auth, async (req, res) => {
   }
 }) 
 
-router.get('/users/:id', async(req, res) => {
-  const _id = req.params.id
-
-  try {
-    const user =  await User.findById(_id)
-    if(!user){
-      return res.status(404).send()
-    }
-    res.send(user)
-  } catch (error) {
-    res.status(500).send()
-  }
-})
-
 router.post('/users', async(req, res) => {
   const user = new User(req.body)
 
@@ -69,7 +55,7 @@ router.post('/users/login', async(req, res) => {
   }
 })
 
-router.patch('/users/:id', async(req, res) => {
+router.patch('/users/me', auth, async(req, res) => {
   const updates = Object.keys(req.body)
   const allowedUpdates = ["name", "email", "password", "age"]
   const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -79,29 +65,26 @@ router.patch('/users/:id', async(req, res) => {
   }
 
   try {
-    const user = await User.findById(req.params.id)
+    // const user = await User.findById(req.params.id)
 
-    updates.forEach((update) => user[update] = req.body[update])
-    await user.save()
+    updates.forEach((update) => req.user[update] = req.body[update])
+    await req.user.save()
 
-    if(!user){
-      return res.status(404).send()
-    }
+    // if(!user){
+    //   return res.status(404).send()
+    // }
 
-    res.send(user)
+    res.send(req.user)
 
   } catch (error) {
     res.status(400).send(error)
   }
 })
 
-router.delete('/users/:id', async(req, res) => {
+router.delete('/users/me', auth, async(req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id)
-    if(!user){
-      return res.status(404).send()
-    }
-    res.send(user)
+    await req.user.remove()
+    res.send(req.user)
   } catch (error) {
     res.status(500).send(error)
   }
